3ed615c568946ea934ccfa4158e0798752ad3b22
jeongho <qkrjh0904@gmail.com>
2023-07-12 23:50:07 +0900

    MockMvc, MockBean 추가

---
+++ src/test/java/sample/cafekiosk/spring/api/product/controller/ProductControllerTest.java
@@ -0,0 +1,20 @@
+package sample.cafekiosk.spring.api.product.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import sample.cafekiosk.spring.api.product.service.ProductService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(controllers = ProductController.class)
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+
+}
