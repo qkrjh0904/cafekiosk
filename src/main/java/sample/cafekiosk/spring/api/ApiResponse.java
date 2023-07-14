061773fc44bd3f3fc5430e532d9d654c68b78ea4
jeongho <qkrjh0904@gmail.com>
2023-07-14 23:56:10 +0900

    ApiResponse 추가

---
+++ src/main/java/sample/cafekiosk/spring/api/ApiResponse.java
@@ -0,0 +1,28 @@
+package sample.cafekiosk.spring.api;
+
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.OK;
+
+public class ApiResponse<T> {
+
+    private int code;
+    private HttpStatus status;
+    private String message;
+    private T data;
+
+    public ApiResponse(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus status, T data) {
+        return new ApiResponse<>(status, status.name(), data);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(OK, OK.name(), data);
+    }
+}
--- src/main/java/sample/cafekiosk/spring/api/product/controller/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/product/controller/ProductController.java
@@ -1,10 +1,12 @@
 package sample.cafekiosk.spring.api.product.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.ApiResponse;
 import sample.cafekiosk.spring.api.product.model.CreateProductRq;
 import sample.cafekiosk.spring.api.product.model.ProductRs;
 import sample.cafekiosk.spring.api.product.service.ProductService;
@@ -18,8 +20,8 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    public void createProduct(@RequestBody CreateProductRq rq) {
-        productService.createProduct(rq);
+    public ApiResponse<ProductRs> createProduct(@RequestBody @Validated CreateProductRq rq) {
+        return ApiResponse.ok(productService.createProduct(rq));
     }
 
     @GetMapping("/api/v1/products/selling")
