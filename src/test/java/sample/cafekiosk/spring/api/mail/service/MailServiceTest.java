d65380b94d441502040fbfb9c6d647405917491d
jeongho <qkrjh0904@gmail.com>
2023-07-21 23:33:35 +0900

    feat: BDDMockito

--- src/test/java/sample/cafekiosk/spring/api/mail/service/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/mail/service/MailServiceTest.java
@@ -4,9 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.api.mail.repository.MailSendHistoryRepository;
 import sample.cafekiosk.spring.domain.entity.MailSendHistory;
@@ -14,14 +14,17 @@ import sample.cafekiosk.spring.mail.MailSendClient;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MailServiceTest {
 
-    @Spy    // 기능이 많아 일부는 실제 객체를 쓰고싶은 경우 spy 를 사용
+//    @Spy    // 기능이 많아 일부는 실제 객체를 쓰고싶은 경우 spy 를 사용
+//    private MailSendClient mailSendClient;
+
+    @Mock
     private MailSendClient mailSendClient;
 
     @Mock
@@ -39,11 +42,16 @@ class MailServiceTest {
 
 //        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
 
-//        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
-//                .thenReturn(true);
-        doReturn(true)
-                .when(mailSendClient)
-                .sendEmail(anyString(), anyString(), anyString(), anyString());
+        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(true);
+
+        // when 으로 시작되는게 이상해서 BDD Mockito를 만들었음.
+        BDDMockito.given(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
+
+//        doReturn(true)
+//                .when(mailSendClient)
+//                .sendEmail(anyString(), anyString(), anyString(), anyString());
 
 
         // when
