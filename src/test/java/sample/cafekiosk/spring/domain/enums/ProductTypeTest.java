b1f1292b7939cc13962fd211b8a5dd43325a8b7b
jeongho <qkrjh0904@gmail.com>
2023-07-28 23:30:16 +0900

    feat: @ParameterizedTest

--- src/test/java/sample/cafekiosk/spring/domain/enums/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/enums/ProductTypeTest.java
@@ -2,7 +2,11 @@ package sample.cafekiosk.spring.domain.enums;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,7 +15,26 @@ class ProductTypeTest {
     @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
     @CsvSource({"HANDMADE,false", "BOTTLE,true", "BAKERY,true"})
     @ParameterizedTest
-    void test(ProductType productType, boolean expected) {
+    void containsStockType1(ProductType productType, boolean expected) {
+        // when
+        boolean result = ProductType.containsStockType(productType);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideProductTypesForCheckingStockType() {
+        return Stream.of(
+                Arguments.of(ProductType.HANDMADE, false),
+                Arguments.of(ProductType.BOTTLE, true),
+                Arguments.of(ProductType.BAKERY, true)
+        );
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @MethodSource("provideProductTypesForCheckingStockType")
+    @ParameterizedTest
+    void containsStockType2(ProductType productType, boolean expected) {
         // when
         boolean result = ProductType.containsStockType(productType);
 
